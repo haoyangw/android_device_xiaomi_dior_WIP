7f33c1460f73b2b873a3c8db31a7c8de6b5dd9ed
haoyangw <haoyangw@gmail.com>
2015-07-17 17:09:15 +0800

    Finally fixed camerawrapper build errors cleanly

--- camera/Android.mk
+++ camera/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
+    CameraWrapper.cpp #\
+    #CameraParametersExtra.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
@@ -11,7 +12,8 @@ LOCAL_C_INCLUDES += \
     system/media/camera/include
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.msm8226
+#LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
